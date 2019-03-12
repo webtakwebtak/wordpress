d267438dd24e14580bf4a9a729c4d8702e0fd02e
Hans Kellerhuis <webtak@gmail.com>
2019-03-12 15:01:38 +0100

    Added addiotional configs

--- config/wp-config.php
+++ config/wp-config.php
@@ -1,41 +1,26 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
- */
+
+$dotenv = Dotenv\Dotenv::create(dirname(__DIR__));
+$dotenv->load();
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'composer-example' );
+define( 'DB_NAME', $_ENV['DB_NAME']);
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', $_ENV['DB_USER'] );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Correo@@@' );
+define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', $_ENV['DB_HOST'] );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', $_ENV['DB_CHARSET'] );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE',$_ENV['DB_COLLATE']);
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +31,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         $_ENV['AUTH_KEY'] );
+define( 'SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY'] );
+define( 'LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY'] );
+define( 'NONCE_KEY',        $_ENV['NONCE_KEY'] );
+define( 'AUTH_SALT',        $_ENV['AUTH_SALT'] );
+define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
+define( 'LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT'] );
+define( 'NONCE_SALT',       $_ENV['NONCE_SALT'] );
 
 /**#@-*/
 
@@ -63,7 +48,7 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = $_ENV['DB_PREFIX'];
 
 /**
  * For developers: WordPress debugging mode.
@@ -78,6 +63,23 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
+
+/**
+ * Additional application variables
+ *
+ */
+switch ($_ENV['WP_ENV']) {
+    case "production":
+        require_once 'environments/production.php';
+        break;
+    case "staging":
+        require_once 'environments/staging.php';
+        break;
+    case "development":
+        require_once 'environments/development.php';
+        break;
+}
+
 
 /* That's all, stop editing! Happy publishing. */
 
