7620da93b795f9a6d402654bf6d971ff1fb30cbe
Hans Kellerhuis <webtak@gmail.com>
2019-07-12 11:39:53 +0200

    Added dbhost to exec Put execution in function

--- script.php
+++ script.php
@@ -4,7 +4,7 @@
  * It remove default data and adds default settings
  * When done delete file
  * */
-//JAAAA
+
 require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
@@ -47,20 +47,20 @@ addNewSalts();
 
 
 function activateTheme() {
-    $sql = "UPDATE `wp_options` SET `option_value` = 'wp-theme' WHERE `option_name` = 'template';UPDATE `wp_options` SET `option_value` = 'wp-theme' WHERE `option_name` = 'stylesheet';UPDATE `wp_options` SET `option_value` = 'Webtak thema' WHERE `option_name` = 'current_theme';UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'theme_switched';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "UPDATE wp_options SET option_value = 'wp-theme' WHERE option_name = 'template';";
+    executeSQL($sql);
     echo "Custom theme activated\n";
 }
 
 function removeComments() {
-    $sql = "TRUNCATE `wp_commentmeta`;TRUNCATE `wp_comments`;";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "TRUNCATE wp_commentmeta;TRUNCATE wp_comments;";
+    executeSQL($sql);
     echo "Comments removed\n";
 }
 
 function removePosts() {
-    $sql = "TRUNCATE `wp_postmeta`;TRUNCATE `wp_posts`;";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "TRUNCATE wp_postmeta;TRUNCATE wp_posts;";
+    executeSQL($sql);
     echo "Posts removed\n";
 }
 
@@ -70,34 +70,34 @@ function deleteDirectory($dir) {
 }
 
 function shutDownComments() {
-    $sql = "UPDATE `wp_options` SET `option_value` = NULL WHERE `option_name` = 'show_avatars';UPDATE `wp_options` SET `option_value` = NULL WHERE `option_name` = 'default_pingback_flag';UPDATE `wp_options` SET `option_value` = 'closed' WHERE `option_name` = 'default_ping_status';UPDATE `wp_options` SET `option_value` = 'closed' WHERE `option_name` = 'default_comment_status';UPDATE `wp_options` SET `option_value` = NULL WHERE `option_name` = 'comments_notify';UPDATE `wp_options` SET `option_value` = NULL WHERE `option_name` = 'moderation_notify';UPDATE `wp_options` SET `option_value` = '1' WHERE `option_name` = 'comment_moderation';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'comment_max_links';UPDATE `wp_options` SET `option_value` = '1' WHERE `option_name` = 'close_comments_for_old_posts';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'close_comments_days_old';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'thread_comments_depth';UPDATE `wp_options` SET `option_value` = NULL WHERE `option_name` = 'page_comments';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'comments_per_page';UPDATE `wp_options` SET `option_value` = '1' WHERE `option_name` = 'comment_registration';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "UPDATE wp_options SET option_value = NULL WHERE option_name = 'show_avatars';UPDATE wp_options SET option_value = NULL WHERE option_name = 'default_pingback_flag';UPDATE wp_options SET option_value = 'closed' WHERE option_name = 'default_ping_status';UPDATE wp_options SET option_value = 'closed' WHERE option_name = 'default_comment_status';UPDATE wp_options SET option_value = NULL WHERE option_name = 'comments_notify';UPDATE wp_options SET option_value = NULL WHERE option_name = 'moderation_notify';UPDATE wp_options SET option_value = '1' WHERE option_name = 'comment_moderation';UPDATE wp_options SET option_value = '0' WHERE option_name = 'comment_max_links';UPDATE wp_options SET option_value = '1' WHERE option_name = 'close_comments_for_old_posts';UPDATE wp_options SET option_value = '0' WHERE option_name = 'close_comments_days_old';UPDATE wp_options SET option_value = '0' WHERE option_name = 'thread_comments_depth';UPDATE wp_options SET option_value = NULL WHERE option_name = 'page_comments';UPDATE wp_options SET option_value = '0' WHERE option_name = 'comments_per_page';UPDATE wp_options SET option_value = '1' WHERE option_name = 'comment_registration';";
+    executeSQL($sql);
     echo "Comments shutdown \n";
 }
 
 function emptyWriteSettings() {
-    $sql ="UPDATE `wp_options` SET `option_value` = '1' WHERE `option_name` = 'default_category';UPDATE `wp_options` SET `option_value` = '1' WHERE `option_name` = 'default_email_category';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'default_link_category';UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'mailserver_url';UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'mailserver_port';UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'mailserver_login';UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'mailserver_pass';UPDATE `wp_options` SET `option_value` = '' WHERE `option_name` = 'ping_sites';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql ="UPDATE wp_options SET option_value = '1' WHERE option_name = 'default_category';UPDATE wp_options SET option_value = '1' WHERE option_name = 'default_email_category';UPDATE wp_options SET option_value = '0' WHERE option_name = 'default_link_category';UPDATE wp_options SET option_value = '' WHERE option_name = 'mailserver_url';UPDATE wp_options SET option_value = '0' WHERE option_name = 'mailserver_port';UPDATE wp_options SET option_value = '' WHERE option_name = 'mailserver_login';UPDATE wp_options SET option_value = '' WHERE option_name = 'mailserver_pass';UPDATE wp_options SET option_value = '' WHERE option_name = 'ping_sites';";
+    executeSQL($sql);
     echo "Write settings applied shutdown \n";
 }
 
 
 function discourageSearchEngines() {
-    $sql = "UPDATE `wp_options` SET `option_value` = '0' WHERE `option_name` = 'blog_public';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "UPDATE wp_options SET option_value = '0' WHERE option_name = 'blog_public';";
+    executeSQL($sql);
     echo "Search engines discouragd \n";
 }
 
 function setPermalinkStructure() {
-    $sql = "UPDATE `wp_options` SET `option_value` = '/%postname%/' WHERE `option_name` = 'permalink_structure';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "UPDATE wp_options SET option_value = '/%postname%/' WHERE option_name = 'permalink_structure';";
+    executeSQL($sql);
     echo "Permalink structure changed\n";
 }
 
 function activatePlugins(){
     $statement = 'a:2:{i:0;s:34:"advanced-custom-fields-pro/acf.php";i:1;s:33:"classic-editor/classic-editor.php";}';
-    $sql = "UPDATE `wp_options` SET `option_value` = '".addslashes($statement)."' WHERE `option_name` = 'active_plugins';";
-    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+    $sql = "UPDATE wp_options SET option_value = '".addslashes($statement)."' WHERE option_name = 'active_plugins';";
+    executeSQL($sql);
     echo "Plugins activated\n";
 }
 
@@ -133,4 +133,6 @@ function generateSalt($max = 66) {
     return $salt;
 }
 
-
+function executeSQL($sql) {
+    $cmd = shell_exec('mysql --user='.$_ENV['DB_USER'].' --host='.$_ENV['DB_HOST'].' --password='.$_ENV['DB_PASSWORD'].' '.$_ENV['DB_NAME'].' -e "'.$sql.'"');
+}
